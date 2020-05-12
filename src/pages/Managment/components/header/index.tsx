66926f1a139f5b6bcfc29446c1383b6784f87e7b
Alexandr Golovchuk <a.golovchuk@gmail.com>
2020-05-12 10:23:30 +0300

    update styles on management panel for tree and lists

--- src/pages/Managment/components/header/index.tsx
+++ src/pages/Managment/components/header/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
+import cx from 'classnames';
 import styles from './header.module.css';
 
 interface Props {
   title: string;
   onCreate?: () => void;
+  isSticky?: boolean;
 }
 
-function Header({ title, onCreate }: Props) {
+function Header({ title, onCreate, isSticky }: Props) {
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, { [styles.sticky]: isSticky })}>
       <h2 className={styles.title}>{title}</h2>
       {
         typeof onCreate === 'function' ? (
